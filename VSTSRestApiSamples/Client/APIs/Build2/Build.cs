35baf1f6be0f5a85578fbd8c33fe25ef82ba1aff
Dan Hellem <dan_hellem@hotmail.com>
2016-04-27 10:29:15 -0700

    Started Builds

---
+++ VSTSRestApiSamples/Client/APIs/Build2/Build.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using VstsRestApiSamples.Client.Helpers;
+using VstsRestApiSamples.ViewModels.Build;
+
+namespace VstsRestApiSamples.Client.APIs.Build2
+{
+    public class Build
+    {
+        private string _account;
+        private string _login;
+
+        public Build(IAuth auth)
+        {
+            _account = auth.Account;
+            _login = auth.Login;
+        }
+
+        public BuildGetListofBuildDefinitionsResponse.Definitions GetListOfBuildDefinitions(string project)
+        {
+            BuildGetListofBuildDefinitionsResponse.Definitions viewModel = new BuildGetListofBuildDefinitionsResponse.Definitions();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_account);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _login);
+
+                HttpResponseMessage response = client.GetAsync(project + "/_apis/build/definitions?api-version=2.0").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    viewModel = response.Content.ReadAsAsync<BuildGetListofBuildDefinitionsResponse.Definitions>().Result;
+                }
+
+                viewModel.HttpStatusCode = response.StatusCode;
+
+                return viewModel;
+            }
+        }
+    }
+}
