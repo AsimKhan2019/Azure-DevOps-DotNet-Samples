1d89b82c196cbc759473c21695a7bd0d67993e46
Dan Hellem <dahellem@microsoft.com>
2017-01-13 11:41:04 -0800

    Recycle Bin

--- VSTSRestApiSamples.UnitTests/WorkItemTracking/RecycleBinTest.cs
+++ VSTSRestApiSamples.UnitTests/WorkItemTracking/RecycleBinTest.cs
@@ -60,7 +60,7 @@ namespace VstsRestApiSamples.Tests.WorkItemTracking
         }
 
         [TestMethod, TestCategory("REST API")]
-        public void WorkItemTracking_RecycleBin_RestoreDeletedItem_Success()
+        public void WorkItemTracking_RecycleBin_RestoreItem_Success()
         {
             // arrange
             WorkItems workItemsRequest = new WorkItems(_configuration);
@@ -69,18 +69,109 @@ namespace VstsRestApiSamples.Tests.WorkItemTracking
             // act
             WorkItemPatchResponse.WorkItem createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
             WorkItemPatchResponse.WorkItem deleteResponse = workItemsRequest.DeleteWorkItem(createResponse.id.ToString());
-            GetItemFromRecycleBinResponse.WorkItem getDeletedItemResponse = recyclebinRequest.GetDeletedItem(_configuration.Project, createResponse.id.ToString());
-            System.Net.HttpStatusCode restoreResponse = recyclebinRequest.RestoreWorkItem(_configuration.Project, createResponse.id.ToString());
+            GetRestoredWorkItemResponse.WorkItem restoreResponse = recyclebinRequest.RestoreItem(createResponse.id.ToString());
 
             ////get restored item
             GetWorkItemExpandAllResponse.WorkItem getRestoredItemResponse = workItemsRequest.GetWorkItem(createResponse.id.ToString());
 
             //assert
             Assert.AreEqual(HttpStatusCode.OK, createResponse.HttpStatusCode);
-            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.HttpStatusCode);
-            Assert.AreEqual(HttpStatusCode.OK, getDeletedItemResponse.HttpStatusCode);
-            Assert.AreEqual(HttpStatusCode.NoContent, restoreResponse);
+            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.HttpStatusCode);            
+            Assert.AreEqual(HttpStatusCode.OK, restoreResponse.HttpStatusCode);
             Assert.AreEqual(HttpStatusCode.OK, getRestoredItemResponse.HttpStatusCode);
+
+            workItemsRequest = null;
+            recyclebinRequest = null;
+        }
+
+        [TestMethod, TestCategory("REST API")]
+        public void WorkItemTracking_RecycleBin_RestoreMultipleItems_Success()
+        {
+            // arrange
+            WorkItems workItemsRequest = new WorkItems(_configuration);
+            RecycleBin recyclebinRequest = new RecycleBin(_configuration);
+            WorkItemPatchResponse.WorkItem createResponse;
+            string[] ids = new string[3]; 
+
+            // act
+            createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
+            ids[0] = createResponse.id.ToString();
+
+            createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
+            ids[1] = createResponse.id.ToString();
+
+            createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
+            ids[2] = createResponse.id.ToString();
+
+            foreach(var id in ids)
+            {
+                var deleteResponse = workItemsRequest.DeleteWorkItem(id);
+            }
+
+            var respond = recyclebinRequest.RestoreMultipleWorkItems(ids);
+                    
+            //assert
+            Assert.AreEqual(HttpStatusCode.OK, createResponse.HttpStatusCode);          
+            
+            workItemsRequest = null;
+            recyclebinRequest = null;
+        }
+
+        [TestMethod, TestCategory("REST API")]
+        public void WorkItemTracking_RecycleBin_PermenentlyDeletedItem_Success()
+        {
+            // arrange
+            WorkItems workItemsRequest = new WorkItems(_configuration);
+            RecycleBin recyclebinRequest = new RecycleBin(_configuration);
+
+            // act
+            WorkItemPatchResponse.WorkItem createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
+            WorkItemPatchResponse.WorkItem deleteResponse = workItemsRequest.DeleteWorkItem(createResponse.id.ToString());
+            HttpStatusCode permDeleteResponse = recyclebinRequest.PermenentlyDeleteItem(createResponse.id.ToString());
+            
+         
+            ////get delete item
+            GetWorkItemExpandAllResponse.WorkItem getDeletedWorkItem = workItemsRequest.GetWorkItem(createResponse.id.ToString());
+
+            //assert
+            Assert.AreEqual(HttpStatusCode.OK, createResponse.HttpStatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, deleteResponse.HttpStatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, permDeleteResponse);
+            Assert.AreEqual(HttpStatusCode.NoContent, getDeletedWorkItem.HttpStatusCode);
+
+            workItemsRequest = null;
+            recyclebinRequest = null;
+        }
+
+        [TestMethod, TestCategory("REST API")]
+        public void WorkItemTracking_RecycleBin_PermenentlyDeleteMultipleItems_Success()
+        {
+            // arrange
+            WorkItems workItemsRequest = new WorkItems(_configuration);
+            RecycleBin recyclebinRequest = new RecycleBin(_configuration);
+            WorkItemPatchResponse.WorkItem createResponse;
+            string[] ids = new string[3];
+
+            // act
+            createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
+            ids[0] = createResponse.id.ToString();
+
+            createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
+            ids[1] = createResponse.id.ToString();
+
+            createResponse = workItemsRequest.CreateWorkItem(_configuration.Project);
+            ids[2] = createResponse.id.ToString();
+
+            foreach (var id in ids)
+            {
+                var deleteResponse = workItemsRequest.DeleteWorkItem(id);
+            }
+
+            GetRestoreMultipleWorkItemsResponse.Items response = recyclebinRequest.PeremenentlyDeleteMultipleItems(ids);
+
+            //assert
+            Assert.AreEqual(HttpStatusCode.OK, createResponse.HttpStatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, response.HttpStatusCode);
 
             workItemsRequest = null;
             recyclebinRequest = null;
--- VSTSRestApiSamples/WorkItemTracking/RecycleBin.cs
+++ VSTSRestApiSamples/WorkItemTracking/RecycleBin.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using VstsRestApiSamples.ViewModels;
 using VstsRestApiSamples.ViewModels.WorkItemTracking;
 
 namespace VstsRestApiSamples.WorkItemTracking
@@ -60,6 +63,37 @@ namespace VstsRestApiSamples.WorkItemTracking
                 if (response.IsSuccessStatusCode)
                 {
                     viewModel = response.Content.ReadAsAsync<GetItemFromRecycleBinResponse.WorkItem>().Result;
+                }               
+
+                viewModel.HttpStatusCode = response.StatusCode;
+
+                return viewModel;
+            }
+        }
+
+        public GetRestoredWorkItemResponse.WorkItem RestoreItem(string id)
+        {
+            GetRestoredWorkItemResponse.WorkItem viewModel = new GetRestoredWorkItemResponse.WorkItem();
+
+            var patchDocument = new {
+                IsDeleted = false
+            };
+            
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _credentials);
+                
+                var patchValue = new StringContent(JsonConvert.SerializeObject(patchDocument), Encoding.UTF8, "application/json"); 
+
+                var method = new HttpMethod("PATCH");
+                var request = new HttpRequestMessage(method, _configuration.UriString + "_apis/wit/recyclebin/" + id + "?api-version=3.0-preview") { Content = patchValue };
+                var response = client.SendAsync(request).Result;  
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    viewModel = response.Content.ReadAsAsync<GetRestoredWorkItemResponse.WorkItem>().Result;
                 }
                 else
                 {
@@ -74,9 +108,30 @@ namespace VstsRestApiSamples.WorkItemTracking
             }
         }
 
-        public System.Net.HttpStatusCode RestoreWorkItem(string project, string id)
+        public GetRestoreMultipleWorkItemsResponse.Items RestoreMultipleWorkItems(string[] ids)
         {
-            WorkItemPatchResponse.WorkItem viewModel = new WorkItemPatchResponse.WorkItem();
+            GetRestoreMultipleWorkItemsResponse.Items viewModel = new GetRestoreMultipleWorkItemsResponse.Items();
+            WorkItemBatchPost.BatchRequest[] postDocument = new WorkItemBatchPost.BatchRequest[3];
+            Dictionary<string, string> headers = new Dictionary<string, string>() {
+                { "Content-Type", "application/json-patch+json" }
+            };
+
+            Object[] postBody = new Object[1];
+            postBody[0] = new { op = "replace", path = "/IsDeleted", value = "false" };
+            var i = 0;
+
+            foreach(var id in ids)
+            {
+                postDocument[i] = new WorkItemBatchPost.BatchRequest
+                {
+                    method = "PATCH",
+                    uri = "/_apis/wit/recyclebin/" + id + "?api-version=3.0-preview",
+                    headers = headers,
+                    body = postBody
+                };
+
+                i = i + 1;
+            };                     
 
             using (var client = new HttpClient())
             {
@@ -84,11 +139,93 @@ namespace VstsRestApiSamples.WorkItemTracking
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _credentials);
 
+                var postValue = new StringContent(JsonConvert.SerializeObject(postDocument), Encoding.UTF8, "application/json");
+
+                var method = new HttpMethod("POST");
+                var request = new HttpRequestMessage(method, _configuration.UriString + "_apis/wit/$batch?api-version=3.0-preview") { Content = postValue };
+                var response = client.SendAsync(request).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    viewModel = response.Content.ReadAsAsync<GetRestoreMultipleWorkItemsResponse.Items>().Result;
+                }               
+
+                viewModel.HttpStatusCode = response.StatusCode;
+
+                return viewModel;
+            }
+        }
+
+        public HttpStatusCode PermenentlyDeleteItem(string id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _credentials);
+                              
                 var method = new HttpMethod("DELETE");
-                var request = new HttpRequestMessage(method, _configuration.UriString + project + "/_apis/wit/recyclebin/" + id + "?restore=true&api-version=3.0-preview");
-                var response = client.SendAsync(request).Result;              
+                var request = new HttpRequestMessage(method, _configuration.UriString + "_apis/wit/recyclebin/" + id + "?api-version=3.0-preview");
+                var response = client.SendAsync(request).Result;
+
+                if (! response.IsSuccessStatusCode)
+                {
+                    dynamic responseForInvalidStatusCode = response.Content.ReadAsAsync<dynamic>();
+                    Newtonsoft.Json.Linq.JContainer msg = responseForInvalidStatusCode.Result;                 
+                }
 
                 return response.StatusCode;
+            }
+        }
+
+        public GetRestoreMultipleWorkItemsResponse.Items PeremenentlyDeleteMultipleItems(string[] ids)
+        {
+            GetRestoreMultipleWorkItemsResponse.Items viewModel = new GetRestoreMultipleWorkItemsResponse.Items();
+            WorkItemBatchPost.BatchRequest[] postDocument = new WorkItemBatchPost.BatchRequest[3];
+            Dictionary<string, string> headers = new Dictionary<string, string>() {
+                { "Content-Type", "application/json-patch+json" }
+            };
+
+            var i = 0;           
+
+            foreach (var id in ids)
+            {
+                postDocument[i] = new WorkItemBatchPost.BatchRequest
+                {
+                    method = "DELETE",
+                    uri = "/_apis/wit/recyclebin/" + id + "?api-version=3.0-preview",
+                    headers = headers                  
+                };
+
+                i = i + 1;
+            };
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _credentials);
+
+                var postValue = new StringContent(JsonConvert.SerializeObject(postDocument), Encoding.UTF8, "application/json");
+
+                var method = new HttpMethod("POST");
+                var request = new HttpRequestMessage(method, _configuration.UriString + "_apis/wit/$batch?api-version=3.0-preview") { Content = postValue };
+                var response = client.SendAsync(request).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    viewModel = response.Content.ReadAsAsync<GetRestoreMultipleWorkItemsResponse.Items>().Result;
+                }
+                else
+                {
+                    dynamic responseForInvalidStatusCode = response.Content.ReadAsAsync<dynamic>();
+                    Newtonsoft.Json.Linq.JContainer msg = responseForInvalidStatusCode.Result;
+                    viewModel.Message = msg.ToString();
+                }
+
+                viewModel.HttpStatusCode = response.StatusCode;
+
+                return viewModel;
             }
         }
     }
