aac9b02dbdd5b7e04e94d85af0fd3394e6093938
Dan Hellem <dahellem@microsoft.com>
2017-01-24 13:14:44 -0800

    area and interation tree sample for rest

---
+++ VSTSRestApiSamples.UnitTests/WorkItemTracking/ClassificationNodesSamplesTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VstsRestApiSamples.WorkItemTracking;
+using VstsRestApiSamples.ViewModels.WorkItemTracking;
+using System.Net;
+using System.Collections.Generic;
+
+namespace VstsRestApiSamples.Tests.WorkItemTracking
+{
+    [TestClass]
+    public class ClassificationNodesSamplesTest
+    {
+        private IConfiguration _configuration = new Configuration();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            InitHelper.GetConfiguration(_configuration);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _configuration = null;
+        }
+
+        [TestMethod, TestCategory("REST API")]
+        public void WorkItemTracking_Nodes_Samples_GetAreaTree_Success()
+        {
+            // arrange
+            ClassificationNodesSamples request = new ClassificationNodesSamples(_configuration);
+
+            // act
+            List<string> response = request.GetAreaTree(_configuration.Project);
+
+            //assert
+            Assert.IsNotNull(response);
+           
+            request = null;
+        }
+
+        [TestMethod, TestCategory("REST API")]
+        public void WorkItemTracking_Nodes_Samples_GetIterationTree_Success()
+        {
+            // arrange
+            ClassificationNodesSamples request = new ClassificationNodesSamples(_configuration);
+
+            // act
+            List<string> response = request.GetIterationTree(_configuration.Project);
+
+            //assert
+            Assert.IsTrue(response.Count > 0);
+
+            request = null;
+        }
+    }
+}
