449adcc267f7606d472dbce103dcce60cb2baadf
Matt Cooper <mattc@xbox.com>
2017-05-17 10:09:10 -0400

    added list branches sample

---
+++ Microsoft.TeamServices.Samples.Client/Git/GitSampleHelpers.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Linq;
+
+namespace Microsoft.TeamServices.Samples.Client.Git
+{
+    public class GitSampleHelpers
+    {
+        public static GitRepository FindAnyRepository(ClientSampleContext context, Guid projectId)
+        {
+            GitRepository repo;
+            if (!FindAnyRepository(context, projectId, out repo))
+            {
+                throw new Exception("No repositories available. Create a repo in this project and run the sample again.");
+            }
+
+            return repo;
+        }
+
+        private static bool FindAnyRepository(ClientSampleContext context, Guid projectId, out GitRepository repo)
+        {
+            // Check if we already have a repo loaded
+            if (!context.TryGetValue<GitRepository>("$someRepo", out repo))
+            {
+                VssConnection connection = context.Connection;
+                GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+
+                using (new ClientSampleHttpLoggerOutputSuppression())
+                {
+                    // Check if an ID was already set (this could have been provided by the caller)
+                    Guid repoId;
+                    if (!context.TryGetValue<Guid>("repositoryId", out repoId))
+                    {
+                        // Get the first repo
+                        repo = gitClient.GetRepositoriesAsync(projectId).Result.FirstOrDefault();
+                    }
+                    else
+                    {
+                        // Get the details for this repo
+                        repo = gitClient.GetRepositoryAsync(repoId.ToString()).Result;
+                    }
+                }
+
+                if (repo != null)
+                {
+                    context.SetValue<GitRepository>("$someRepo", repo);
+                }
+                else
+                {
+                    // create a project here?
+                    throw new Exception("No repos available for running the sample.");
+                }
+            }
+
+            return repo != null;
+        }
+    }
+}
---
+++ Microsoft.TeamServices.Samples.Client/Git/RefsSample.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.TeamServices.Samples.Client.Git
+{
+    [ClientSample(GitWebApiConstants.AreaName, "refs")]
+    public class RefsSample : ClientSample
+    {
+        [ClientSampleMethod]
+        public IEnumerable<GitRef> ListBranches()
+        {
+            VssConnection connection = this.Context.Connection;
+            GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+
+            Guid projectId = ClientSampleHelpers.FindAnyProject(this.Context).Id;
+            Guid repoId = GitSampleHelpers.FindAnyRepository(this.Context, projectId).Id;
+
+            List<GitRef> refs = gitClient.GetRefsAsync(repoId, filter: "heads/").Result;
+            
+            foreach(GitRef gitRef in refs)
+            {
+                Console.WriteLine("{0} {1} {2}", gitRef.Name, gitRef.ObjectId, gitRef.Url);
+            }
+
+            return refs;            
+        }
+    }
+}
