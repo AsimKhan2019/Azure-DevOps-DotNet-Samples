40d512189114b392bfa4284b027c0685827f2305
Will Smythe <wismythe@microsoft.com>
2017-06-01 16:04:56 -0400

    Refactor repo layout; Add service hook security samples

---
+++ UtilSamples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.TeamServices.Samples.ServiceHooks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = (args.Length > 0 ? args[0] : null);
+
+            if (command == null || args.Length < 2)
+            {
+                Console.WriteLine("Usage: Microsoft.TeamServices.Samples.ServiceHooks [command] [collection URL]");
+            }
+            else if (String.Equals(command, "restore", StringComparison.InvariantCultureIgnoreCase))
+            {
+                RestoreManagePermissionsToProjectAdminGroups r = new RestoreManagePermissionsToProjectAdminGroups();
+                r.Run(new Uri(args[1]));
+            }
+            else if(String.Equals(command, "restrict", StringComparison.InvariantCultureIgnoreCase))
+            {
+                RestrictPermissionsToOneGroup r = new RestrictPermissionsToOneGroup();
+                r.Run(new Uri(args[1]));
+            }
+        }
+    }
+}
