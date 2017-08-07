ff00b8a489989ae4b20876260b31f0d4be9ac1a5
Matt Cooper <mattc@xbox.com>
2017-08-07 13:33:17 -0400

    list ACLs

---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Security/AccessControlListsSample.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.Services.Security;
+using Microsoft.VisualStudio.Services.Security.Client;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.TeamServices.Samples.Client.Security
+{
+    [ClientSample(LocationResourceIds.SecurityServiceArea, "accesscontrollists")]
+    public class AccessControlListsSample : ClientSample
+    {
+        [ClientSampleMethod]
+        public IEnumerable<AccessControlList> ListAllGitAcls()
+        {
+            VssConnection connection = this.Context.Connection;
+            SecurityHttpClient securityClient = connection.GetClient<SecurityHttpClient>();
+
+            IEnumerable<AccessControlList> acls = securityClient.QueryAccessControlListsAsync(
+                // in a real app, you should get this value via securityClient.QuerySecurityNamespacesAsync
+                GitSecurityNamespace,
+                string.Empty,
+                descriptors: null,
+                includeExtendedInfo: false,
+                recurse: true).Result;
+
+            Console.WriteLine("token | inherit? | count of ACEs");
+            Console.WriteLine("------+----------+--------------");
+            foreach (AccessControlList acl in acls)
+            {
+                Console.WriteLine("{0} | {1} | {2} ACEs", acl.Token, acl.InheritPermissions, acl.AcesDictionary.Count());
+            }
+
+            return acls;
+        }
+
+        private Guid GitSecurityNamespace = Guid.Parse("2e9eb7ed-3c0a-47d4-87c1-0ffdd275fd87");
+    }
+}
