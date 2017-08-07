e007df35132602781ed538d7832b967993c3e2b8
Matt Cooper <mattc@xbox.com>
2017-08-07 16:14:11 -0400

    add ACEs

--- ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Security/AccessControlListsSample.cs
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Security/AccessControlListsSample.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.Services.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Microsoft.TeamServices.Samples.Client.Security
 {
@@ -24,16 +25,87 @@ namespace Microsoft.TeamServices.Samples.Client.Security
                 includeExtendedInfo: false,
                 recurse: true).Result;
 
+            // we'll store one interesting ACE to expand in a later method
+            bool storedAcl = false;
+
             Console.WriteLine("token | inherit? | count of ACEs");
             Console.WriteLine("------+----------+--------------");
             foreach (AccessControlList acl in acls)
             {
                 Console.WriteLine("{0} | {1} | {2} ACEs", acl.Token, acl.InheritPermissions, acl.AcesDictionary.Count());
+
+                if (!storedAcl && acl.Token.Length > "repoV2/".Length)
+                {
+                    this.Context.SetValue(this.StoredAclKey, acl);
+                    storedAcl = true;
+                }
             }
 
             return acls;
         }
 
+        [ClientSampleMethod]
+        public void ExpandGitAcl()
+        {
+            bool hasInterestingAcl = this.Context.TryGetValue(this.StoredAclKey, out AccessControlList acl);
+            if (!hasInterestingAcl)
+            {
+                Console.WriteLine("no interesting ACLs found");
+                return;
+            }
+
+            // get the details for Git permissions
+            VssConnection connection = this.Context.Connection;
+            SecurityHttpClient securityClient = connection.GetClient<SecurityHttpClient>();
+
+            IEnumerable<SecurityNamespaceDescription> namespaces;
+
+            using (new ClientSampleHttpLoggerOutputSuppression())
+            {
+                namespaces = securityClient.QuerySecurityNamespacesAsync(this.GitSecurityNamespace).Result;
+            }
+            SecurityNamespaceDescription gitNamespace = namespaces.First();
+
+            Dictionary<int, string> permission = new Dictionary<int, string>();
+            foreach(ActionDefinition actionDef in gitNamespace.Actions)
+            {
+                permission[actionDef.Bit] = actionDef.DisplayName;
+            }
+
+            // use the Git permissions data to expand the ACL
+            Console.WriteLine("Expanding ACL for {0} ({1} ACEs)", acl.Token, acl.AcesDictionary.Count());
+            foreach (var kvp in acl.AcesDictionary)
+            {
+                // in the key-value pair, Key is an identity and Value is an ACE (access control entry)
+                // allow and deny are bit flags indicating which permissions are allowed/denied
+                Console.WriteLine("Identity {0} | allow {1} | deny {2}", kvp.Key, kvp.Value.Allow, kvp.Value.Deny);
+                Console.WriteLine("  Allowed: {0}", UnpackPermissionBits(kvp.Value.Allow, permission));
+                Console.WriteLine("  Denied: {0}", UnpackPermissionBits(kvp.Value.Deny, permission));
+            }
+
+            return;
+        }
+
+        private string UnpackPermissionBits(int bitsSet, Dictionary<int, string> bitMeanings)
+        {
+            List<string> permissionStrings = new List<string>();
+            foreach(var kvp in bitMeanings)
+            {
+                if ((bitsSet & kvp.Key) == kvp.Key)
+                {
+                    permissionStrings.Add(kvp.Value);
+                }
+            }
+
+            string value = string.Join(", ", permissionStrings.ToArray());
+            if (string.IsNullOrEmpty(value))
+            {
+                return "<none>";
+            }
+            return value;
+        }
+
         private Guid GitSecurityNamespace = Guid.Parse("2e9eb7ed-3c0a-47d4-87c1-0ffdd275fd87");
+        private string StoredAclKey = "example_git_acl";
     }
 }
