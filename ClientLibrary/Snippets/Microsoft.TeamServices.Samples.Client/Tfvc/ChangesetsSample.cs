b1710089d9075e8d0af83479ae011578f1806eb3
Matt Cooper <mattc@xbox.com>
2017-08-09 08:02:47 -0400

    add changesetchanges

---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Tfvc/ChangesetsSample.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.TeamServices.Samples.Client.Tfvc
+{
+    [ClientSample(TfvcConstants.AreaName, "changesets")]
+    public class ChangesetsSample : ClientSample
+    {
+        [ClientSampleMethod]
+        public IEnumerable<TfvcChangesetRef> ListChangesets()
+        {
+            VssConnection connection = this.Context.Connection;
+            TfvcHttpClient tfvcClient = connection.GetClient<TfvcHttpClient>();
+
+            IEnumerable<TfvcChangesetRef> changesets = tfvcClient.GetChangesetsAsync(top: 10).Result;
+
+            foreach (TfvcChangesetRef changeset in changesets)
+            {
+                Console.WriteLine("{0} by {1}: {2}", changeset.ChangesetId, changeset.Author.DisplayName, changeset.Comment ?? "<no comment>");
+            }
+
+            return changesets;
+        }
+    }
+}
