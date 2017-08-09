49be36d50ffac313b4c77cda7076be5cf0ddad42
Matt Cooper <mattc@xbox.com>
2017-08-09 08:21:23 -0400

    create changeset

--- ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Tfvc/ChangesetsSample.cs
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Tfvc/ChangesetsSample.cs
@@ -26,5 +26,65 @@ namespace Microsoft.TeamServices.Samples.Client.Tfvc
 
             return changesets;
         }
+
+        [ClientSampleMethod]
+        public TfvcChangesetRef CreateChange()
+        {
+            VssConnection connection = this.Context.Connection;
+            TfvcHttpClient tfvcClient = connection.GetClient<TfvcHttpClient>();
+
+            string projectName = ClientSampleHelpers.FindAnyProject(this.Context).Name;
+            DateTime time = DateTime.UtcNow;
+            string destinationFilePath = string.Format("$/{0}/example-file-{1}.txt", projectName, time.ToString("yyyy-MM-dd-HH-mm-ss-ff"));
+            string destinationFileContents = string.Format("File contents as of {0}", time);
+
+            TfvcChangeset changeset = new TfvcChangeset()
+            {
+                Changes = new[]
+                {
+                    new TfvcChange()
+                    {
+                        ChangeType = VersionControlChangeType.Add,
+                        Item = new TfvcItem()
+                        {
+                            Path = destinationFilePath,
+                            ContentMetadata = new FileContentMetadata()
+                            {
+                                Encoding = Encoding.UTF8.WindowsCodePage,
+                                ContentType = "text/plain",
+                            }
+                        },
+                        NewContent = new ItemContent()
+                        {
+                            Content = destinationFileContents,
+                            ContentType = ItemContentType.RawText,
+                        },
+                    },
+                },
+                Comment = "(sample) Adding a new changeset via API",
+            };
+
+            try
+            {
+                TfvcChangesetRef changesetRef = tfvcClient.CreateChangesetAsync(changeset).Result;
+                Console.WriteLine("{0} by {1}: {2}", changesetRef.ChangesetId, changesetRef.Author.DisplayName, changesetRef.Comment ?? "<no comment>");
+                return changesetRef;
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("Something went wrong, could not create TFVC changeset.");
+                if (e.InnerException.Message.Contains(projectName))
+                {
+                    Console.WriteLine("This may mean project \"{0}\" isn't configured for TFVC.", projectName);
+                    Console.WriteLine("Add a TFVC repo to the project, then try this sample again.");
+                }
+                else
+                {
+                    Console.WriteLine(e.InnerException.Message);
+                }
+            }
+
+            return null;
+        }
     }
 }
