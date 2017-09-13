d94caf37b1349a107b1aac2a9208d68de2f93816
Justin Marks (MSFT) <jmarks@microsoft.com>
2017-09-13 14:32:09 -0700

    Refactor based on previous sample

--- ClientLibrary/Quickstarts/dotnet/GraphQuickStarts/Program.cs
+++ ClientLibrary/Quickstarts/dotnet/GraphQuickStarts/Program.cs
@@ -1,36 +1,104 @@
-﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
-using Microsoft.VisualStudio.Services.Client;
-using Microsoft.VisualStudio.Services.Common;
-using Microsoft.VisualStudio.Services.Graph.Client;
-using Microsoft.VisualStudio.Services.WebApi;
-using System;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GraphQuickStarts
 {
     class Program
     {
-        //============= Config [Edit these with your settings] =====================
-        internal const string vstsCollectionUrl = "https://myaccount.visualstudio.com"; //change to the URL of your VSTS account; NOTE: This must use HTTPS
-        // internal const string vstsCollectioUrl = "http://myserver:8080/tfs/DefaultCollection" alternate URL for a TFS collection
-        //==========================================================================        
-
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            VssConnection connection = new VssConnection(new Uri(vstsCollectionUrl), new VssClientCredentials());
-            GraphHttpClient graphClient = connection.GetClient<GraphHttpClient>();
-
-            // Get the first page of Users
-            PagedGraphUsers users = graphClient.GetUsersAsync().Result;
-
-            // If there are more than a page's worth of users, continue retrieving users from the server
-            string continuationToken = users.ContinuationToken.FirstOrDefault();
-            while (continuationToken != null)
+            if (args.Length == 0)
             {
-                users = graphClient.GetUsersAsync(continuationToken: continuationToken).Result;
-                // DO SOMETHING WITH THE USERS LIST
+                ShowUsage();
+                return 0;
+            }
 
-                continuationToken = users.ContinuationToken.FirstOrDefault();
+            string connectionUrl, token = "";
+
+            try
+            {
+                CheckArguments(args, out connectionUrl, out token);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                ShowUsage();
+                return -1;
+            }
+
+            try
+            {
+                Console.WriteLine("Executing Graph quick start samples...");
+                Console.WriteLine("");
+
+                //instantiate objects & execute
+                Samples.EnumerateUsers objUsers = new Samples.EnumerateUsers(connectionUrl, token);
+
+                //execute the client lib code. If you want to run the direct http calls then adjust (see below)
+                objUsers.RunEnumerateUsersUsingClientLib();
+
+                objUsers = null;
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to run the sample: " + ex.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Runs the Graph Quick Start samples on a Team Services account or Team Foundation Server instance.");
+            Console.WriteLine("");
+            Console.WriteLine("These samples are to provide you the building blocks of using the REST API's in Identity.");
+            Console.WriteLine("Examples are written using the .NET client library and using direct HTTP calls. We recommend, that");
+            Console.WriteLine("whenever possible, you use the .NET client library.");
+            Console.WriteLine("");
+            Console.WriteLine("Arguments:");
+            Console.WriteLine("");
+            Console.WriteLine("  /url:fabrikam.vssps.visualstudio.com /token:personalaccesstoken");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+
+        private static void CheckArguments(string[] args, out string connectionUrl, out string token)
+        {
+            connectionUrl = null;
+            token = null;
+
+            Dictionary<string, string> argsMap = new Dictionary<string, string>();
+            foreach (var arg in args)
+            {
+                if (arg[0] == '/' && arg.IndexOf(':') > 1)
+                {
+                    string key = arg.Substring(1, arg.IndexOf(':') - 1);
+                    string value = arg.Substring(arg.IndexOf(':') + 1);
+
+                    switch (key)
+                    {
+                        case "url":
+                            connectionUrl = value;
+                            break;
+
+                        case "token":
+                            token = value;
+                            break;
+                            
+                        default:
+                            throw new ArgumentException("Unknown argument", key);
+                    }
+                }
+            }
+
+            if (connectionUrl == null || token == null)
+            {
+                throw new ArgumentException("Missing required arguments");
             }
         }
     }
