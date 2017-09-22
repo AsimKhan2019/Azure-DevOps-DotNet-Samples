1c51f6a73f43f36ffde4d438f14fc6cfa11c1457
Ruslan Semenov <rsemenov@microsoft.com>
2017-09-22 16:10:20 -0400

    Add Pull Request statuses samples

--- ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Git/GitSampleHelpers.cs
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Git/GitSampleHelpers.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace Microsoft.TeamServices.Samples.Client.Git
 {
@@ -69,6 +70,156 @@ namespace Microsoft.TeamServices.Samples.Client.Git
             return $"{ChooseNamePart()}.{ChooseNamePart()}.{ChooseNamePart()}";
         }
 
+        public static GitPullRequest CreatePullRequest(ClientSampleContext context, GitRepository repo)
+        {
+            VssConnection connection = context.Connection;
+            GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+
+            using (new ClientSampleHttpLoggerOutputSuppression())
+            {
+                GitPullRequest pullRequest = gitClient.GetPullRequestsAsync(
+                    repo.Id,
+                    new GitPullRequestSearchCriteria()
+                    {
+                        Status = PullRequestStatus.Active
+                    }).Result.FirstOrDefault();
+                
+                if (pullRequest == null)
+                {
+                    pullRequest = CreatePullRequestInternal(context, repo, gitClient);
+                }
+
+                return pullRequest;
+            }
+        }
+
+        public static GitPullRequest AbandonPullRequest(ClientSampleContext context, GitRepository repo, int pullRequestId)
+        {
+            VssConnection connection = context.Connection;
+            GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+
+            using (new ClientSampleHttpLoggerOutputSuppression())
+            {
+                // clean up after ourselves (and in case logging is on, don't log these calls)
+                ClientSampleHttpLogger.SetSuppressOutput(context, true);
+
+                // abandon the PR
+                GitPullRequest updatedPr = new GitPullRequest()
+                {
+                    Status = PullRequestStatus.Abandoned,
+                };
+
+                var pullRequest = gitClient.UpdatePullRequestAsync(updatedPr, repo.Id, pullRequestId).Result;
+
+                return pullRequest;
+            }
+        }
+
+        public static GitPullRequestStatus CreatePullRequestStatus(ClientSampleContext context, Guid repositoryId, int pullRequestId, int? iterationId = null)
+        {
+            VssConnection connection = context.Connection;
+            GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+
+            using (new ClientSampleHttpLoggerOutputSuppression())
+            {
+                GitPullRequestStatus status = GenerateSampleStatus(iterationId);
+                GitPullRequestStatus createdStatus = gitClient.CreatePullRequestStatusAsync(status, repositoryId, pullRequestId).Result;
+
+                return createdStatus;
+            }
+        }
+
+        public static string WithoutRefsPrefix(string refName)
+        {
+            if (!refName.StartsWith("refs/"))
+            {
+                throw new Exception("The ref name should have started with 'refs/' but it didn't.");
+            }
+            return refName.Remove(0, "refs/".Length);
+        }
+
+        public static GitPullRequestStatus GenerateSampleStatus(int? iterationId = null, bool includeProperties = false)
+        {
+            var status = new GitPullRequestStatus
+            {
+                Context = new GitStatusContext
+                {
+                    Name = $"sample-status-{Rng.Next(1, 5)}",
+                    Genre = "vsts-samples"
+                },
+                TargetUrl = "http://fabrikam-fiber-inc.com/CI/builds/1",
+                State = GitStatusState.Succeeded,
+                Description = "Sample status succeeded",
+                IterationId = iterationId
+            };
+
+            if (includeProperties)
+            {
+                status.Properties = new PropertiesCollection();
+                status.Properties["sampleId"] = Rng.Next(1, 10);
+                status.Properties["customInfo"] = "Custom status information";
+                status.Properties["startedDateTime"] = DateTime.UtcNow;
+                status.Properties["weight"] = 1.75;
+                status.Properties["bytes"] = Encoding.UTF8.GetBytes("this is sample base64 encoded string");
+                status.Properties["globalId"] = Guid.NewGuid();
+            }
+
+            return status;
+        }
+
+        private static GitPullRequest CreatePullRequestInternal(ClientSampleContext context, GitRepository repo, GitHttpClient gitClient)
+        {
+            // we need a new branch with changes in order to create a PR
+            // first, find the default branch
+            string defaultBranchName = WithoutRefsPrefix(repo.DefaultBranch);
+            GitRef defaultBranch = gitClient.GetRefsAsync(repo.Id, filter: defaultBranchName).Result.First();
+
+            // next, craft the branch and commit that we'll push
+            GitRefUpdate newBranch = new GitRefUpdate()
+            {
+                Name = $"refs/heads/vsts-api-sample/{ChooseRefsafeName()}",
+                OldObjectId = defaultBranch.ObjectId,
+            };
+            string newFileName = $"{ChooseItemsafeName()}.md";
+            GitCommitRef newCommit = new GitCommitRef()
+            {
+                Comment = "Add a sample file",
+                Changes = new GitChange[]
+                {
+                    new GitChange()
+                    {
+                        ChangeType = VersionControlChangeType.Add,
+                        Item = new GitItem() { Path = $"/vsts-api-sample/{newFileName}" },
+                        NewContent = new ItemContent()
+                        {
+                            Content = "# Thank you for using VSTS!",
+                            ContentType = ItemContentType.RawText,
+                        },
+                    }
+                },
+            };
+
+            // create the push with the new branch and commit
+            GitPush push = gitClient.CreatePushAsync(new GitPush()
+            {
+                RefUpdates = new GitRefUpdate[] { newBranch },
+                Commits = new GitCommitRef[] { newCommit },
+            }, repo.Id).Result;
+
+            // finally, create a PR
+            var pr = gitClient.CreatePullRequestAsync(new GitPullRequest()
+            {
+                SourceRefName = newBranch.Name,
+                TargetRefName = repo.DefaultBranch,
+                Title = $"Add {newFileName} (from VSTS REST samples)",
+                Description = "Adding this file from the pull request samples",
+            },
+            repo.Id).Result;
+
+            return pr;
+
+        }
+
         private static string ChooseNamePart()
         {
             if (WordList == null)
@@ -98,15 +249,6 @@ namespace Microsoft.TeamServices.Samples.Client.Git
             }
 
             WordList = words;
-        }
-
-        public static string WithoutRefsPrefix(string refName)
-        {
-            if (!refName.StartsWith("refs/"))
-            {
-                throw new Exception("The ref name should have started with 'refs/' but it didn't.");
-            }
-            return refName.Remove(0, "refs/".Length);
         }
 
         private static List<string> WordList;
