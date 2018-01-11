3cacb2ce8dc3ebb75d27220df37dd89102dff2f3
Justin Marks (MSFT) <jmarks@microsoft.com>
2018-01-11 13:44:05 -0800

    Updating ADAL logic

--- ClientLibrary/Quickstarts/dotnet/MaterializeUserQuickStarts/Program.cs
+++ ClientLibrary/Quickstarts/dotnet/MaterializeUserQuickStarts/Program.cs
@@ -1,34 +1,29 @@
 ﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using AadAuthenticationContext = Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext;
 
 namespace MaterializeUserQuickStarts
 {
     class Program
     {
-
         const string userName = "user@domain.com";
-        const string password = "Password";
-        const string accountName = "VSTSACCOUNT";
+        const string password = "P@ssw0rd!"; 
+        const string accountName = "account";
 
         static void Main(string[] args)
         {
-            var authenticationContext = new AadAuthenticationContext("https://login.windows.net/common", validateAuthority: true);
-            var authenticationResultTask = authenticationContext.AcquireTokenAsync(
-                "https://graph.windows.net",
-                "872cd9fa-d31f-45e0-9eab-6e460a02d1f1",
-                new UserPasswordCredential(userName, password));
-            var authenticationResult = authenticationResultTask.Result;
-            var bearerAuthHeader = new AuthenticationHeaderValue("Bearer", authenticationResultTask.Result.AccessToken);
-
+            AuthenticationContext ctx = GetAuthenticationContext(null);
+            AuthenticationResult result = ctx.AcquireTokenAsync("499b84ac-1321-427f-aa17-267ca6975798", "872cd9fa-d31f-45e0-9eab-6e460a02d1f1", new UserPasswordCredential(userName, password)).Result;
+            var bearerAuthHeader = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+            
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri(String.Format("https://{0}.vssps.visualstudio.com/", accountName)); //NOTE this is the SPS endpoint for the account
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Add("User-Agent", "VstsRestApiSamples");
+                client.DefaultRequestHeaders.Add("User-Agent", "MaterializeUserQuickStarts");
                 client.DefaultRequestHeaders.Add("X-TFS-FedAuthRedirect", "Suppress");
                 client.DefaultRequestHeaders.Authorization = bearerAuthHeader;
 
@@ -38,7 +33,7 @@ namespace MaterializeUserQuickStarts
                 // check to see if we have a succesfull respond
                 if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine("\tSuccesfully materizlized the user");
+                    Console.WriteLine("Successfully materizlized the user");
                 }
                 else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
                 {
@@ -49,6 +44,24 @@ namespace MaterializeUserQuickStarts
                     Console.WriteLine("{0}:{1}", response.StatusCode, response.ReasonPhrase);
                 }
             }
+        }
+
+        private static AuthenticationContext GetAuthenticationContext(string tenant)
+        {
+            AuthenticationContext ctx = null;
+            if (tenant != null)
+                ctx = new AuthenticationContext("https://login.microsoftonline.com/" + tenant);
+            else
+            {
+                ctx = new AuthenticationContext("https://login.windows.net/common");
+                if (ctx.TokenCache.Count > 0)
+                {
+                    string homeTenant = ctx.TokenCache.ReadItems().First().TenantId;
+                    ctx = new AuthenticationContext("https://login.microsoftonline.com/" + homeTenant);
+                }
+            }
+
+            return ctx;
         }
     }
 }
