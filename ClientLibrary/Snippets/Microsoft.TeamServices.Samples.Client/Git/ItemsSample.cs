d4fbe712d9e9cac390292a7e3940cb90c57b9fa5
Matt Cooper <mattc@xbox.com>
2018-05-15 09:45:11 -0400

    add Git.GetItem sample

--- ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Git/ItemsSample.cs
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Git/ItemsSample.cs
@@ -4,8 +4,6 @@ using Microsoft.VisualStudio.Services.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.TeamServices.Samples.Client.Git
 {
@@ -30,6 +28,27 @@ namespace Microsoft.TeamServices.Samples.Client.Git
             }
 
             return items;            
+        }
+
+        [ClientSampleMethod]
+        public GitItem GetItem()
+        {
+            VssConnection connection = this.Context.Connection;
+            GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+
+            TeamProjectReference project = ClientSampleHelpers.FindAnyProject(this.Context);
+            GitRepository repo = GitSampleHelpers.FindAnyRepository(this.Context, project.Id);
+
+            // get a filename we know exists
+            string filename = gitClient.GetItemsAsync(repo.Id, scopePath: "/", recursionLevel: VersionControlRecursionType.OneLevel).Result
+                .Where(o => o.GitObjectType == GitObjectType.Blob).FirstOrDefault().Path;
+
+            // retrieve the contents of the file
+            GitItem item = gitClient.GetItemAsync(repo.Id, filename, includeContent: true).Result;
+
+            Console.WriteLine("File {0} at commit {1} is of length {2}", filename, item.CommitId, item.Content.Length);
+
+            return item;
         }
     }
 }
